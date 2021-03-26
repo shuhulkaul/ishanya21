dd0f4122423dc21ccdf471de0e399371af2e5ac4
Shuhul Kaul <shuhulkaul22@gmail.com>
2021-03-26 22:51:12 +0530

    added video

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-$(document).on("click mousemove", "body", function (e) {
+function changeVal(a) {
+  if (a == 0) {
+    return 660;
+  } else {
+    return 0;
+  }
+}
+
+$(document).on("click mousemove", ".parent", function (e) {
   var x = e.clientX;
   var y = e.clientY;
-  var newposX = x-660;
+  var newposX = x - changeVal(0);
   var newposY = y;
   // $(".container").css(
   //   "transform",
@@ -9,8 +17,28 @@ $(document).on("click mousemove", "body", function (e) {
   // );
   $(".container").css(
     "-webkit-mask-position",
-    newposX + "px " + newposY + "px" 
+    newposX + "px " + newposY + "px"
   );
 });
-// - 920
-// - 580
+
+function myOverFunction(e) {
+  document.getElementById("container").style.setProperty("webkitMaskImage", "");
+  document
+    .getElementsByTagName("img")[0]
+    .setAttribute("src", "./img/Group 437(FILL).svg");
+  document
+    .getElementsByTagName("video")[0]
+    .setAttribute("style", "-webkit-mask-image: ''");
+}
+
+function myLeaveFunction(e) {
+  document
+    .getElementsByTagName("img")[0]
+    .setAttribute("src", "./img/Group 437.svg");
+  document
+    .getElementsByTagName("video")[0]
+    .setAttribute(
+      "style",
+      "-webkit-mask-image: url(https://kaul-bucket.s3.us-east-2.amazonaws.com/L.svg);"
+    );
+}
